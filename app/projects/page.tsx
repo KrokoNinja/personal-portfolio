a0949c7b25d2a945ae48529a3c4507b0c3ec588c
KrokoNinja <lenni.pfundtner@gmail.com>
2024-02-25 13:10:09 +0100

    add projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,38 @@
+'use client'
 
+import ProjectCard from "@/components/ProjectCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { projectData } from "@/lib/data";
+import { useState } from "react";
+
+const uniqueCategories = ['all projects', ...new Set(projectData.map((project) => project.category))];
 
 export default function Projects() {
-    return <div>projects</div>;
+
+    const [categories, setCategories] = useState(uniqueCategories);
+    const [category, setCategory] = useState('all projects');
+
+    const filteredProjects = projectData.filter((project) => {
+        return category === 'all projects' ? project : project.category === category;
+    });
+
+    return <section className="min-h-screen pt-12">
+        <div className="container mx-auto">
+            <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">My Projects</h2>
+            <Tabs defaultValue={category} className="mb-24 xl:mb-48">
+                <TabsList className="w-full grid h-full md:grid-cols-3 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+                    {categories.map((category, index) => {
+                        return <TabsTrigger value={category} key={index} className="capitalize w-[162px] md:w-auto" onClick={() => setCategory(category)}>{category}</TabsTrigger>
+                    })}
+                </TabsList>
+                <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+                    {filteredProjects.map((project, index) => {
+                        return <TabsContent value={category} key={index}>
+                            <ProjectCard project={project} />
+                        </TabsContent>
+                    })}
+                </div>
+            </Tabs>
+        </div>
+    </section>;
 }
---
+++ lib/data.ts
@@ -0,0 +1,28 @@
+import Project from "@/typings";
+
+export const projectData = [
+    {
+        image: "/work/product-view-card-component.webp",
+        category: "HTML/CSS",
+        title: "Product View Card Component",
+        description: "This is a solution to the Product preview card component challenge on Frontend Mentor.",
+        link: '/',
+        github: '/',
+    },
+    {
+        image: "/work/fem-huddle-landing-page.webp",
+        category: "HTML/CSS",
+        title: "Huddle Landing Page",
+        description: "This is a solution to the Huddle landing page challenge on Frontend Mentor.",
+        link: '/',
+        github: '/',
+    },
+    {
+        image: "/work/product-view-card-component.webp",
+        category: "Next JS",
+        title: "Personal Portfolio",
+        description: "This is my own portfolio website built with Next JS, shadcn/ui and Tailwind CSS.",
+        link: '/',
+        github: '/',
+    },
+] as Project[];
