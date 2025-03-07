c7ecf0896604cc18cc5db0796bb2b558f105cdc1
KrokoNinja <lenni.pfundtner@gmail.com>
2025-03-07 16:02:38 +0100

    add: underconstruction section for current project

---
+++ components/UnderConstruction.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import { RiGithubFill } from "react-icons/ri";
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { ExternalLink } from "lucide-react";
+
+export default function UnderConstruction() {
+  return (
+    <div className="bg-secondary p-6 rounded-lg mb-8">
+      <div className="flex gap-8 flex-col xl:flex-row">
+        <div className="flex flex-col gap-4">
+          <h3 className="text-2xl font-bold">🚧 Under Construction 🚧</h3>
+          <p className="text-muted-foreground">
+            Durch meine Passion für Darts bin ich auf das Problem gestoßen, dass
+            es auf dem Mac keine gute Möglichkeit gibt, seine Spiele und
+            Statistiken zu tracken und gleichzeitig seine Scores mit der
+            Tastatur während dem spielen eingeben zu können. Darum habe ich mich
+            dazu entschieden, eine Web App zu erstellen. Aktuell ist die App
+            noch in der Entwicklung und ich arbeite an der Implementierung von
+            Features.
+          </p>
+          <div className="flex gap-4 xl:flex-col">
+            <Link
+              href="https://github.com/KrokoNinja/darts-tracker"
+              className={cn(buttonVariants(), "flex items-center gap-2")}
+            >
+              <RiGithubFill size={32} />
+              Link zum Repository
+            </Link>
+            <Button disabled>
+              <Link
+                href="#"
+                className={cn(buttonVariants(), "flex items-center gap-2")}
+              >
+                <ExternalLink size={32} />
+                Link zur Website (Noch nicht verfügbar)
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <Image
+          src="/work/darts-tracker-screenshot.png"
+          alt="Darts Tracker Screenshot"
+          width={1907}
+          height={933}
+          className="xl:max-w-[750px]"
+        />
+      </div>
+    </div>
+  );
+}
--- components/Work.tsx
+++ components/Work.tsx
@@ -1,51 +1,59 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
-import {Pagination} from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
 import ProjectCard from "./ProjectCard";
 
 import { projectData } from "@/lib/data";
 import { ArrowRightIcon } from "lucide-react";
-
+import UnderConstruction from "./UnderConstruction";
 export default function Work() {
-    return <section className="relative mb-12 xl:mb-48">
-        <div className="container mx-auto flex gap-x-8 flex-col xl:flex-row">
-            <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
-                <h2 className="section-title mb-4">Meine Projekte</h2>
-                <p className="subtitle mb-8">Hier sind ein paar meiner Projekte aufgelistet. Mein größtes Projekt bisher? Casecobra! Mit Hilfe von <a href="https://www.github.com/joschan21" className="border-b border-zinc-400">joschan21</a> und seinem 12 Stunden (!) YouTube Video habe ich es geschafft, diese Seite zu bauen und hatte die Möglichkeit sehr viele neue Dinge zu lernen.</p>
-                <Link href="/projects/">
-                    <Button className="mt-8 gap-x-4 group">
-                        Zu allen Projekten
-                        <ArrowRightIcon size={18} className="group-hover:translate-x-2 transition-all" />
-                    </Button>
-                </Link>
-            </div>
-            <div className="xl:max-w-[1000px] right-20 top-0">
-                <Swiper className="h-[480px]" slidesPerView={1} breakpoints={{
-                    640: {
-                        slidesPerView: 2,
-                    },
-                }}
-                spaceBetween={30}
-                modules={[Pagination]}
-                pagination={{
-                    "clickable": true
-                }}
-                >
-                    {projectData.slice(0,4).map((project, index) => {
-                        return <SwiperSlide key={index}>
-                            <ProjectCard project={project} />
-                        </SwiperSlide>
-                    })}
-                </Swiper>
-            </div>
+  return (
+    <section className="relative mb-12 xl:mb-48">
+      <div className="container mx-auto flex gap-x-8 flex-col">
+        <h2 className="section-title mb-8 mx-auto">Meine Projekte</h2>
+        <UnderConstruction />
+        <div className="xl:max-w-[1000px] mx-auto">
+          <Swiper
+            className="h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{
+              clickable: true,
+            }}
+          >
+            {projectData.slice(0, 4).map((project, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <ProjectCard project={project} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
+        <Link href="/projects/" className="mx-auto">
+          <Button className="mt-8 gap-x-4 group">
+            Zu allen Projekten
+            <ArrowRightIcon
+              size={18}
+              className="group-hover:translate-x-2 transition-all"
+            />
+          </Button>
+        </Link>
+      </div>
     </section>
+  );
 }
