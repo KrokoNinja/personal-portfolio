e59b9a68bb0714f174b84a5dc5167cec9379f7e2
KrokoNinja <lenni.pfundtner@gmail.com>
2025-03-09 13:02:27 +0100

    Update CV

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,43 +1,61 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import {Download, Send} from "lucide-react";
+import { Download, Send } from "lucide-react";
 
-import {
-    RiArrowDownSLine,
-} from 'react-icons/ri';
+import { RiArrowDownSLine } from "react-icons/ri";
 
 import DevImg from "./DevImg";
 import Socials from "./Socials";
 
 export default function Hero() {
-    return (
-        <section className="relative py-12 mb-16 xl:py-24 md:h-[84vh] md:max-h-[700px] xl:max-h-[800px] xl:pt-28 bg-hero bg-no-repeat dark:bg-none bg-bottom bg-cover">
-            <div className="container mx-auto">
-                <div className="flex justify-between gap-x-8">
-                    <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
-                        <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Web Developer</div>
-                        <h1 className="h1 mb-4">Hi, ich bin Lennert</h1>
-                        <p className="subtitle max-w-[490px] mx-auto xl:mx-0">Aktuell studiere ich Informatik an der IU Internationale Hochschule im Fernstudium und lerne nebenbei Frontend-Entwicklung.<br/> Momentan lerne ich React und NextJS und benutze Tailwind CSS und shadcn/ui.<br/> In meiner Freizeit interessiere ich mich neben Programmierung für Darts und Fußball.</p>
-                        <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
-                            <Link href="/contact/">
-                                <Button className="gap-x-2">Contact me <Send size={18}/></Button>
-                            </Link>
-                            <Link href="/hero/Lebenslauf_Lennert_Pfundtner.pdf" download={true} rel="noopener noreferrer">
-                                <Button variant={"secondary"} className="gap-x-2">Download CV <Download size={18}/></Button>
-                            </Link>
-                        </div>
-                        <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0" iconStyles="text-foreground text-[22px] hover:text-primary transition-all" />
-                    </div>
-                    <div className="hidden xl:flex relative">
-                        <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2">
-                            <DevImg containerStyles="bg-hero_shape w-[510px] h-[482px] bg-no-repeat relative bg-bottom" />
-                        </div>
-                    </div>
-                </div>
-                <div className="hidden md:flex absolute left-0 right-0 ml-auto mr-auto w-[30px] bottom-44 md:bottom-12 xl:bottom-5 animate-bounce">
-                    <RiArrowDownSLine className="text-3xl text-primary" />
-                </div>
+  return (
+    <section className="relative py-12 mb-16 xl:py-24 md:h-[84vh] md:max-h-[700px] xl:max-h-[800px] xl:pt-28 bg-hero bg-no-repeat dark:bg-none bg-bottom bg-cover">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Web Developer
             </div>
-        </section>
-    );
+            <h1 className="h1 mb-4">Hi, ich bin Lennert</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Aktuell studiere ich Informatik an der IU Internationale
+              Hochschule im Fernstudium und lerne nebenbei Frontend-Entwicklung.
+              <br /> Momentan lerne ich React und NextJS und benutze Tailwind
+              CSS und shadcn/ui.
+              <br /> In meiner Freizeit interessiere ich mich neben
+              Programmierung für Darts und Fußball.
+            </p>
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact/">
+                <Button className="gap-x-2">
+                  Contact me <Send size={18} />
+                </Button>
+              </Link>
+              <Link
+                href="/hero/Lebenslauf_DE.pdf"
+                download={true}
+                rel="noopener noreferrer"
+              >
+                <Button variant={"secondary"} className="gap-x-2">
+                  Download CV <Download size={18} />
+                </Button>
+              </Link>
+            </div>
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
+          </div>
+          <div className="hidden xl:flex relative">
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2">
+              <DevImg containerStyles="bg-hero_shape w-[510px] h-[482px] bg-no-repeat relative bg-bottom" />
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:flex absolute left-0 right-0 ml-auto mr-auto w-[30px] bottom-44 md:bottom-12 xl:bottom-5 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
 }
